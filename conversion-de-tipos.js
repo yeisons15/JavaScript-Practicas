9ad661718156b6b733ab6c1547fecab05d7c5261
yeisons15 <yeisons15@hotmail.com>
2025-06-08 12:47:38 -0500

    Creación archivo conversion-de-tipos.js

---
+++ conversion-de-tipos.js
@@ -0,0 +1,13 @@
+const string = '42'
+const integer = parseInt(string)
+console.log(integer)
+console.log(typeof integer)
+
+const stringDecimal = '3.14'
+const float = parseFloat(stringDecimal)
+console.log(float)
+console.log(typeof float)
+
+const sum = '5' + 3
+console.log(sum)
+console.log (typeof sum)
