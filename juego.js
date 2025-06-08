0db98f1d75e86e8cdfafa06956e5200e03fb9daa
yeisons15 <yeisons15@hotmail.com>
2025-06-08 12:49:02 -0500

    Creación archivo juego.js

---
+++ juego.js
@@ -0,0 +1,4 @@
+const numeroSecreto = Math.floor(Math.random()*10+1);
+const numeroJugador = parseInt(prompt("Adivina el numero secreto entre 1 - 10"));
+
+console.log(`Este es el número con el que juegas: ${numeroJugador}`);
