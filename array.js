cf99da196628fae85ac1e728c249a549ae23cddf
yeisons15 <yeisons15@hotmail.com>
2025-07-08 22:41:03 -0500

    Se crea ejercicio 1 de arrays

--- array.js
+++ array.js
@@ -7,3 +7,31 @@ console.log(Frutas)
 Frutas.push('Manzana');
 
 console.log(Frutas)
+
+/*
+
+🧪 Nivel 1 – Básico
+🟢 Ejercicio 1: Crear un array de tus comidas favoritas
+Crea un array llamado comidasFavoritas con al menos 4 elementos. Luego:
+
+Muestra el segundo elemento.
+
+Cambia el último por "Sushi".
+
+Agrega "Pizza" al final.
+
+Muestra la longitud del array.
+
+*/
+
+
+
+console.log("---------------")
+const favoriteFoods = ["Bandeja Paisa", "Picada", "Ajiaco", "Pasta"]
+console.log(favoriteFoods)
+console.log(favoriteFoods[1])
+favoriteFoods[3]="Suchi"
+console.log(favoriteFoods)
+favoriteFoods.push("Pizza")
+console.log(favoriteFoods)
+console.log("El tamaño del arreglo es: " + favoriteFoods.length)
