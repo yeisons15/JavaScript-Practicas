85e82aeab5b4d599050a5c56071b3a04c9ef70c0
yeisons15 <yeisons15@hotmail.com>
2025-07-13 20:09:22 -0500

    Ejemplo de un objeto en POO JS

---
+++ JS-POO/JS-POO-1.js
@@ -0,0 +1,11 @@
+const persona = {
+    nombre:"Yeison",
+    edad: 35,
+    direccion: {
+        calle: "Calle 2 # 21-55",
+        Ciudad: "Funza"
+    },
+    saludar() {
+        console.log(`Hola mi nombre es ${persona.nombre}`)
+    }
+}
